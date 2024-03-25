1b9f7b82b63606e543000cf139e517f5524618da
Med Ali Khaled <medalikhaled331@gmail.com>
2024-03-25 16:49:28 +0100

    add commands map

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import "fmt"
+
+type CliCommand struct {
+	Name        string
+	Description string
+	Callback    func() error
+}
+
+func commandHelp() error {
+	return nil
+}
+
+func commandExit() error {
+	return nil
+}
+
 func main() {
+
+	// create a map for available commands, every command is an instance of that struct
+	Commands := map[string]CliCommand{
+		"help": {
+			Name:        "help",
+			Description: "Displays a help message",
+			Callback:    commandHelp,
+		},
+		"exit": {
+			Name:        "exit",
+			Description: "Exit the Pokedex",
+			Callback:    commandExit,
+		},
+	}
+
+	fmt.Printf("%+v", Commands)
+
+	// stdin
+	// inifite loop, break if the stdin has one of the commands name
+	// execute the callback of that command
+
 	println("Hello")
 }
