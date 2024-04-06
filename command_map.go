6be2bb5b68d5c413977c9810c933045ba61434ef
Med Ali Khaled <medalikhaled331@gmail.com>
2024-04-06 18:06:22 +0100

    create package pokeapi

--- command_map.go
+++ command_map.go
@@ -1,47 +1,57 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
-)
+	"time"
 
-type LocationResult struct {
-	Count    int    `json:"count"`
-	Next     string `json:"next"`
-	Previous string `json:"previous"`
-	Results  []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"results"`
-}
+	pokeapi "github.com/MEDALIALPHA331/pokedex/internal/pokeapi"
+)
 
 const POKE_API_URL = "https://pokeapi.co/api/v2"
 
+
+type httpClient struct {
+	client *http.Client
+}
+
+func NewHttpClient() *httpClient {
+	return &httpClient{
+		client: &http.Client{
+			Timeout: time.Minute,
+		},
+	}
+}
+
 func commandMap() error {
-	res, err := http.Get(POKE_API_URL + "/location/?limit=20&offset=20")
+	_, cancel := context.WithTimeout(context.Background(), time.Millisecond*500)
+	defer cancel()
+
+	res, err := http.Get(POKE_API_URL + "/location/")
 
 	if err != nil {
-		log.Fatalf("Error getting pokemons from api: %+v", err)
+		return err
 	}
 
 	if res.Status != "200 OK" {
-		fmt.Println("Couldn't Fetch Pokemon Location Data")
+		return errors.New("couldn't Fetch Pokemon Location Data")
 	}
 
 	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Fatalf("Couldn't Read body: %+v", err)
+		return err
 	}
 
-	var data LocationResult
+	var data pokeapi.LocationAreas
 	json.Unmarshal(body, &data)
 
 	if err != nil {
-		log.Fatalf("%+v", err)
+		return err
 	}
 
 	for _, location := range data.Results {
